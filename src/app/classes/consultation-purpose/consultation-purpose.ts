ae3133d0909dd0f288546ee04e84e51dd90dbe51
keithlim <keithlck96@gmail.com>
2020-10-12 01:37:26 +0800

    format code

--- src/app/classes/consultation-purpose/consultation-purpose.ts
+++ src/app/classes/consultation-purpose/consultation-purpose.ts
@@ -7,8 +7,10 @@ export class ConsultationPurpose {
         consultationPurposeId?: number,
         consultationPurposeName?: string
     ) {
+
         this.consultationPurposeId = consultationPurposeId
         this.consultationPurposeName = consultationPurposeName
+        
     }
 
 }
